015204447e2934d79ef61e3d624f71a72343da40
Shigee <39821453+Shigee@users.noreply.github.com>
2018-06-08 16:12:06 +0900

    空白の新規登録に対してキレる

--- Quiz/src/model/EntryLogic.java
+++ Quiz/src/model/EntryLogic.java
@@ -5,8 +5,17 @@ import dao.AccountDAO;
 public class EntryLogic {
 
 	public boolean execute(NewAccount NewAccount){
-		AccountDAO dao = new AccountDAO();
-		boolean result =dao.createRecord(NewAccount);
-		return result;
+		String name = NewAccount.getUserName();
+		String pass = NewAccount.getPass();
+
+		//名前とパスワードのどちらかが空白の場合falseを返す(リダイレクト)
+		if(name.isEmpty() || pass.isEmpty()){
+			boolean result = false;
+			return result;
+		}else{
+			AccountDAO dao = new AccountDAO();
+			boolean result =dao.createRecord(NewAccount);
+			return result;
+		}
 	}
 }
